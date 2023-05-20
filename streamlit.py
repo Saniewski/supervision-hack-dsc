049c663e09ece33502ba1778ae4be95a6b213a0a
JacJacJr <104026864+JacJacJr@users.noreply.github.com>
2023-05-20 23:54:32 +0200

    Add streamlit, add top-ogloszenia crawler and scrapper
    * Adding a top-ogloszenia hrefs base
    
    * Adding a top-ogloszenia crawler and scrapper
    
    * Ready >1000 rows datasets of descriptions from top-ogloszenia.pl
    
    * Add streamlit first version
    
    * Add scraper with 2 (1 left) scrapers and text visualisator

---
+++ streamlit.py
@@ -0,0 +1,113 @@
+import streamlit as st
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+
+def clean_text(text):
+    cleaned_text = re.sub(r"<.*?>", "", text)
+    cleaned_text = re.sub(r"&#[0-9]+;", "", cleaned_text)
+    cleaned_text = re.sub(r",", " ", cleaned_text)
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text)
+    cleaned_text = cleaned_text.strip()
+    return cleaned_text
+
+def scrape_text(url):
+    options = Options()
+    options.add_argument("--headless")
+    service = Service("path/to/chromedriver")  # Replace with your chromedriver path
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        if 'top-ogloszenia.net' in url:
+            driver.get(url)
+            work_desc = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'h2.product-description'))
+            ).get_attribute('innerHTML')
+            print(work_desc)
+            return clean_text(work_desc)
+        if 'oglaszamy24.pl' in url:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            description_element = soup.find("div", id="adv_desc")
+            work_desc = description_element.text.strip()
+            print(work_desc)
+            return clean_text(work_desc)
+        if 'olx.pl' in url:
+            #sprawdź czy to jest dobrze
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            description_element = soup.find('div', _class='.css-19srbbu')
+            work_desc = description_element.text.strip()
+            print(work_desc)
+            return clean_text(work_desc)
+    except:
+        return False
+    finally:
+        driver.quit()
+
+def fakehunter_predict(text):
+    # Your FakeHunter model prediction logic here
+    import random
+    probability = random.uniform(0, 1)
+    return probability
+
+def assign_conditions(probability):
+    conditions = {
+        "fake": {"text": "OSZUST", "color": "red", "func": st.error},
+        "maybe_fake": {"text": "Jest ryzyko", "color": "yellow", "func": st.warning},
+        "non_fake": {"text": "Nie ma oszustwa", "color": "green", "func": st.success}
+    }
+
+    if probability > 0.85:
+        return conditions["fake"]
+    elif 0.6 <= probability <= 0.85:
+        return conditions["maybe_fake"]
+    else:
+        return conditions["non_fake"]
+
+st.title("FakeHunter - Sprawdzanie ogłoszeń o pracę")
+
+tab = st.sidebar.selectbox("Wybierz zakładkę", ("Wstaw ogłoszenie", "Podaj link"))
+
+if tab == "Wstaw ogłoszenie":
+    text = st.text_area("Wpisz treść ogłoszenia o pracę", height=200)
+
+    if st.button("Sprawdź ogłoszenie"):
+        if text:
+            probability = fakehunter_predict(text)
+            st.markdown(f"**Prawdopodobieństwo oszustwa**: {probability:.2%}")
+            conditions = assign_conditions(probability)
+            if conditions["text"]:
+                conditions["func"](conditions["text"])
+            st.text(f"{probability*100:.2f}%")
+            st.markdown(
+                f'<progress value="{probability}" max="1" style="width: 100%; background-color: {conditions["color"]};"></progress>',
+                unsafe_allow_html=True
+            )
+
+elif tab == "Podaj link":
+    url = st.text_input("Wprowadź link do ogłoszenia o pracę")
+
+    if st.button("Sprawdź link"):
+        if url:
+            text = scrape_text(url)
+            if text:
+                probability = fakehunter_predict(text)
+                st.markdown(text)
+                st.markdown(f"**Prawdopodobieństwo oszustwa**: {probability:.2%}")
+                conditions = assign_conditions(probability)
+                if conditions["text"]:
+                    conditions["func"](conditions["text"])
+                st.text(f"{probability*100:.2f}%")
+                st.markdown(
+                    f'<progress value="{probability}" max="1" style="width: 100%; background-color: {conditions["color"]};"></progress>',
+                    unsafe_allow_html=True
+                )
+            else:
+                st.warning("Nie można zeskrapować tekstu ogłoszenia. Sprawdź wprowadzony link.")
---
+++ topogl_scrapper.py
@@ -0,0 +1,64 @@
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.robotparser import RobotFileParser
+from html import unescape
+import re
+import json
+
+def clean_text(text):
+    cleaned_text = unescape(text)  # Usunięcie oznaczeń HTML
+    cleaned_text = re.sub(r"<.*?>", "", cleaned_text)  # Usunięcie oznaczeń HTML
+    cleaned_text = re.sub(r"&#[0-9]+;", "", cleaned_text)  # Usunięcie odniesień do znaków Unicode
+    cleaned_text = re.sub(r",", " ", cleaned_text)  # Zamiana przecinków na spacje
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text)  # Usunięcie białych znaków (spacje, nowe linie)
+    cleaned_text = cleaned_text.strip()  # Usunięcie białych znaków z początku i końca tekstu
+    return cleaned_text
+
+PATH = "/opt/homebrew/bin/chromedriver"
+
+work_hrefs = pd.read_csv('data/topogl_work_hrefs.csv')
+
+options = Options()
+options.add_argument('--headless') 
+driver = webdriver.Chrome(PATH, options=options)
+
+robot_parser = RobotFileParser()
+robot_parser.set_url('https://top-ogloszenia.net/robots.txt')
+robot_parser.read()
+
+if not robot_parser.can_fetch('*', driver.current_url):
+    print("Strona jest niedostępna dla skryptu.")
+    driver.quit()
+    exit()
+
+with open('data/topogl_work_descriptions.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['id', 'descriptions'])  
+
+descriptions = []
+
+for href in work_hrefs.iloc[:, 0]:
+    href_full = 'https://top-ogloszenia.net/' + href
+    id = href.rsplit("-", 1)[-1]  # Wyciągnięcie id
+
+    driver.get(href_full)
+
+    work_desc = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'h2.product-description'))
+    ).get_attribute('innerHTML')
+    work_desc = clean_text(work_desc)
+    descriptions.append({'id': id, 'description': work_desc})
+
+    with open('data/topogl_work_descriptions.csv', 'a') as csv_f:
+        writer = csv.writer(csv_f)
+        writer.writerow([id, work_desc])
+    with open('data/topogl_work_descriptions.json', 'w', encoding='utf-8') as json_f:
+        json.dump(descriptions, json_f, ensure_ascii=False)
+
+driver.quit()
